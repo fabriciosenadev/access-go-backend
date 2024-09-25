e63804c4d1e0f4e6ff3cd5cc23e911c0772dd56a
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-09-25 15:10:27 -0300

    remove unused endpoint

--- src/guest/guest.controller.ts
+++ src/guest/guest.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, BadRequestException } from '@nestjs/common';
 import { GuestService } from './guest.service';
 import { CreateGuestDto } from './dto/create-guest.dto';
 
@@ -8,11 +8,6 @@ export class GuestController {
 
   @Post()
   create(@Body() createGuestDto: CreateGuestDto) {
-    return this.guestService.create(createGuestDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.guestService.findAll();
+      return this.guestService.create(createGuestDto);
   }
 }
