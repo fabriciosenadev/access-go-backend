49eeccafc0a6873a7d5de256a03daee408f42abc
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-09-25 17:26:44 -0300

    remove unused code

--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Configuração do CORS
-    app.enableCors();
-  // app.enableCors({
-  //   origin: 'https://*/', // Domínio permitido
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Inclua 'OPTIONS'
-  //   allowedHeaders: 'Content-Type, Authorization',
-  //   credentials: true, // Se necessário
-  //   optionsSuccessStatus: 204 // Para navegadores antigos
-  // });
-
-  app.use((req, res, next) => {
-    console.log(`Request Method: ${req.method}, Request URL: ${req.url}`);
-    next();
-  });
-
-  // app.use((req, res, next) => {
-  //   if (req.method === 'OPTIONS') {
-  //     res.header('Access-Control-Allow-Origin', 'https://access-go-frontend.vercel.app');
-  //     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-  //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  //     res.header('Access-Control-Allow-Credentials', 'true');
-  //     return res.status(204).send(); // Respondemos diretamente ao preflight
-  //   }
-  //   next();
-  // });
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
