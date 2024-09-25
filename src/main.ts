d1239df81eecf5806fc24d3642a4df7bf4e8d901
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-09-25 16:03:16 -0300

    change cors config

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // app.enableCors({
-  //   origin: '*',
-  //   methods: 'GET,PUT,POST,OPTIONS',
-  //   allowedHeaders: 'Content-Type, Authorization',
-  // });
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
 
   // Middleware para lidar com requisições preflight manualmente
   // app.use((req, res, next) => {
