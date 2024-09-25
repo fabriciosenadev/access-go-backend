04c536d567d2bb4ae9722d758e914351a04d2096
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-09-25 10:34:02 -0300

    add fonts to vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,9 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+
+/**
+ * FONTES BASE PARA O DEPLOY NA VERCEL
+ * https://medium.com/@jeremy.brunel.fullstack/deploying-a-nestjs-and-redis-app-on-vercel-as-easy-as-pie-eb15e2849717
+ * https://www.youtube.com/watch?v=FZiza5N6BQU
+ */
