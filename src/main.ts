f5566288b03c8fe733d395a1968e58ea144cc69b
Fabricio Sena <39681954+fabriciosenadev@users.noreply.github.com>
2024-09-25 15:49:51 -0300

    add manual treatment to cors

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors({
     origin: '*',
-    methods: 'GET,PUT,POST,OPTIONS'
+    methods: 'GET,PUT,POST,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
   });
+
+  // Middleware para lidar com requisições preflight manualmente
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
   await app.listen(3000);
 }
 bootstrap();
